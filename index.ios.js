78cd3a28f64209cfee1c3cd798fb20539679828c
Wilson Lau <lauwilson90@gmail.com>
2016-11-23 23:30:20 -0800

    change index to explicitly import the initializer when needed.

--- index.ios.js
+++ index.ios.js
@@ -3,12 +3,13 @@
 import { AppRegistry } from 'react-native';
 import SgsWiki from './app/SgsWiki.js';
 
+
 /*
     Bootstrap the realm database construction into the app itself. Since the database
     is pre-populated and will be bundled with the app, this code should never be run
     as part of the normal operating workflow.
 */
-// import initializeData from './app/data/initializer.js';
+// import  { initializeData } from './app/data/initializer.js';
 // initializeData();
 
 AppRegistry.registerComponent('SgsWiki', () => SgsWiki );
